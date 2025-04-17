de57cb10030e6c12523637fb6950812da6d4e033
Seyoung Kulp <hana130sy@gmail.com>
2025-04-17 15:46:49 -0400

    Updated Breadcrumbs icon
    Breadcrumbs icon and minor layout changes in mobile device screen.

--- config/db/structure.sql
+++ config/db/structure.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 17.0
--- Dumped by pg_dump version 17.0
+-- Dumped from database version 17.0 (Postgres.app)
+-- Dumped by pg_dump version 17.0 (Postgres.app)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
