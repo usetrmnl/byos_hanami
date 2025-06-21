98b311639073898912b9fe7b0bbaf7c5261a9dfd
Ben Linsay <blinsay@gmail.com>
2025-06-20 22:19:27 -0400

    add an ALLOWED_SUBNETS setting to allow more IP ranges to connect

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -15,4 +15,12 @@ Rack::Attack.safelist "allow private network" do |request|
   private_subnets.any? { |subnet| subnet.include? request.ip }
 end
 
+allowed_subnets = ENV['ALLOWED_SUBNETS']&.split(',')&.map do |subnet|
+  IPAddr.new(subnet.strip)
+end
+
+Rack::Attack.safelist "allowed subnets" do |request|
+  allowed_subnets.any? { |subnet| subnet.include? request.ip }
+end
+
 Rack::Attack.throttle("requests by IP", limit: 100, period: 60, &:ip)
