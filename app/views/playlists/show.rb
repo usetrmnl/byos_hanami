2649b95c603de01261ab8b6aa637464ea1626c73
Brooke Kuhlmann <brooke@alchemists.io>
2025-07-01 16:29:23 -0600

    Added playlists show resource
    Necessary to render a playlist within the UI.
    
    Milestone: minor

---
+++ app/views/playlists/show.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Views
+    module Playlists
+      # The show view.
+      class Show < Terminus::View
+        expose :playlist
+      end
+    end
+  end
+end
