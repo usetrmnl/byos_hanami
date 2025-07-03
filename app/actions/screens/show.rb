7c65ff7290d7e2f4ae0075afa3d195272dc59f39
Brooke Kuhlmann <brooke@alchemists.io>
2025-07-03 15:00:16 -0600

    Added screens show resource
    Necessary to render an individual screen.
    
    Milestone: minor

---
+++ app/actions/screens/show.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Actions
+    module Screens
+      # The show action.
+      class Show < Terminus::Action
+        include Deps[repository: "repositories.screen"]
+
+        params { required(:id).filled :integer }
+
+        def handle request, response
+          parameters = request.params
+
+          halt :unprocessable_entity unless parameters.valid?
+
+          response.render view, **view_settings(request, parameters)
+        end
+
+        private
+
+        def view_settings request, parameters
+          settings = {screen: repository.find(parameters[:id])}
+
+          settings[:layout] = false if request.env.key? "HTTP_HX_REQUEST"
+          settings
+        end
+      end
+    end
+  end
+end
