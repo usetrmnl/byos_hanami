5067dd03556e81c3f9aa02f9b769865574569a6c
Brooke Kuhlmann <brooke@alchemists.io>
2025-07-18 16:31:35 -0600

    Added playlist item delete action
    Necessary to delete an existing playlist item.
    
    Milestone: minor

--- config/routes.rb
+++ config/routes.rb
@@ -68,6 +68,9 @@ module Terminus
     post "/playlists/:playlist_id/items", to: "playlists.items.create", as: :playlist_item_create
     get "/playlists/:playlist_id/items/edit", to: "playlists.items.edit", as: :playlist_item_edit
     put "/playlists/:playlist_id/items/:id", to: "playlists.items.update", as: :playlist_item_update
+    delete "/playlists/:playlist_id/items/:id",
+           to: "playlists.items.delete",
+           as: :playlist_item_delete
 
     get "/playlists/:id/mirror/edit", to: "playlists.mirror.edit", as: :playlist_mirror_edit
     put "/playlists/:id/mirror", to: "playlists.mirror.update", as: :playlist_mirror_update
