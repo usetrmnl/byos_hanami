d901032d9b4570b5100fec992b77186e31350d6a
Brooke Kuhlmann <brooke@alchemists.io>
2025-07-18 16:24:44 -0600

    Added playlist item new resource
    Necessary to add a item to a playlist. This includes shared form fields and save button partials that will be used by the edit resource (soon to be implemented).
    
    Milestone: minor

--- config/routes.rb
+++ config/routes.rb
@@ -64,6 +64,7 @@ module Terminus
     delete "/playlists/:id", to: "playlists.delete", as: :playlist_delete
 
     get "/playlists/:playlist_id/items", to: "playlists.items.index", as: :playlist_items
+    get "/playlists/:playlist_id/items/new", to: "playlists.items.new", as: :playlist_item_new
 
     get "/playlists/:id/mirror/edit", to: "playlists.mirror.edit", as: :playlist_mirror_edit
     put "/playlists/:id/mirror", to: "playlists.mirror.update", as: :playlist_mirror_update
