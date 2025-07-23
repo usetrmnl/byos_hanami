3643a9a51e5e5e61f2aefda5c840bdea16c166fe
Brooke Kuhlmann <brooke@alchemists.io>
2025-07-23 14:47:25 -0600

    Added device provisioner
    Necessary to handle the setup and configuration of new devices. Supports the following:
    
    * Answers existing device if device exists by MAC Address.
    * Otherwise, creates new device with welcome screen playlist item and makes the playlist item the current item for the playlist.
    
    Milestone: minor

---
+++ app/aspects/devices/provisioner.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require "dry/monads"
+
+module Terminus
+  module Aspects
+    module Devices
+      # Handles the setup and default configuration of new devices.
+      class Provisioner
+        include Deps[
+          "aspects.devices.defaulter",
+          "aspects.screens.welcomer",
+          repository: "repositories.device",
+          playlist_repository: "repositories.playlist",
+          item_repository: "repositories.playlist_item"
+        ]
+        include Dry::Monads[:result]
+
+        def call(mac_address:, **)
+          device = repository.find_by(mac_address:)
+
+          return Success device if device
+
+          device = create(mac_address, **)
+
+          welcomer.call(device).fmap { |screen| configure device, screen }
+        end
+
+        private
+
+        def create(mac_address, **)
+          repository.create defaulter.call.merge!(mac_address:, **)
+        end
+
+        def configure device, screen
+          playlist_id = create_playlist_id device
+          item = item_repository.create_with_position playlist_id:, screen_id: screen.id
+
+          playlist_repository.update playlist_id, current_item_id: item.id
+          repository.update device.id, playlist_id:
+        end
+
+        def create_playlist_id device
+          id = device.friendly_id
+          playlist = playlist_repository.create label: "Device #{id}", name: "device_#{id.downcase}"
+
+          playlist.id
+        end
+      end
+    end
+  end
+end
