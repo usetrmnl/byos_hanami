69d3ca4525b1d7c8b3483e07d1dca8b87f249806
Brooke Kuhlmann <brooke@alchemists.io>
2025-08-12 11:51:52 -0600

    Refactored firmware header contract to be alpha-sorted
    Necessary for faster key lookup.
    
    Issue: 210
    Milestone: patch

--- app/contracts/firmware/header.rb
+++ app/contracts/firmware/header.rb
@@ -8,13 +8,13 @@ module Terminus
         optional(:HTTP_ACCESS_TOKEN).maybe :string
         optional(:HTTP_BATTERY_VOLTAGE).filled :float
         optional(:HTTP_FW_VERSION).filled Types::Version
+        optional(:HTTP_HEIGHT).filled :integer
         optional(:HTTP_HOST).filled :string
         required(:HTTP_ID).filled :string
         optional(:HTTP_REFRESH_RATE).filled :integer
         optional(:HTTP_RSSI).filled :integer
         optional(:HTTP_USER_AGENT).filled :string
         optional(:HTTP_WIDTH).filled :integer
-        optional(:HTTP_HEIGHT).filled :integer
       end
     end
   end
