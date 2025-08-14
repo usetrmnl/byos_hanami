70dcabecbac4a1b2cb906a3aafd158e8b3cc3a9c
Brooke Kuhlmann <brooke@alchemists.io>
2025-08-14 14:33:31 -0600

    Updated screens converter to handle 1-bit BMP and 1/2-bit PNG images
    Necessary to leverage the recently added BMP (1-bit), PNG (1-bit), and PNG (2-bit) screen converters so we can support all known image types.
    
    Milestone: minor

--- app/aspects/screens/converter.rb
+++ app/aspects/screens/converter.rb
@@ -2,58 +2,29 @@
 
 require "dry/monads"
 require "initable"
-require "refinements/array"
 
 module Terminus
   module Aspects
     module Screens
       # Converts image to greyscale BMP image.
-      # :reek:DataClump
       class Converter
-        include Deps[:mini_magick]
-        include Initable[types: TYPES]
+        include Deps[
+          "aspects.screens.converters.bitmap",
+          "aspects.screens.converters.one_bit",
+          "aspects.screens.converters.two_bit"
+        ]
         include Dry::Monads[:result]
 
-        using Refinements::Array
-
         def call model, input_path, output_path
-          output_path.extname.delete_prefix "."
-
-          typed_path_for(output_path, model.mime_type).bind do |typed_output_path|
-            process model, input_path, typed_output_path, output_path
+          case model
+            in mime_type: "image/bmp", bit_depth: 1
+              bitmap.call model, input_path, output_path
+            in mime_type: "image/png", bit_depth: 1
+              one_bit.call model, input_path, output_path
+            in mime_type: "image/png", bit_depth: 2
+              two_bit.call model, input_path, output_path
+            else Failure "Unable to convert image. Unsupported model: #{model.id}."
           end
-        end
-
-        private
-
-        # rubocop:todo Metrics/ParameterLists
-        def process model, input_path, typed_output_path, output_path
-          convert model, input_path, typed_output_path
-          Success output_path
-        rescue MiniMagick::Error => error
-          Failure error.message
-        end
-        # rubocop:enable Metrics/ParameterLists
-
-        def convert model, input_path, typed_output_path
-          mini_magick.convert do |converter|
-            converter << input_path.to_s
-            converter.dither << "FloydSteinberg"     # Popular black-n-white dithering algorithm.
-            converter.remap << "pattern:gray50"
-            converter.depth model.bit_depth
-            converter.colors model.colors
-            converter.strip
-            converter.resize "#{model.dimensions}!"
-            converter << typed_output_path           # Saves to path using specific image type.
-          end
-        end
-
-        def typed_path_for path, extension
-          type = types[extension]
-
-          return Success "#{type}:#{path}" if type
-
-          Failure %(Invalid MIME Type: #{extension.inspect}. Use: #{types.keys.to_usage "or"}.)
         end
       end
     end
