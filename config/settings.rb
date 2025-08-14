dbf20848aa8a4cabecb34dded66c39e6bf19e02e
Brooke Kuhlmann <brooke@alchemists.io>
2025-08-14 11:50:06 -0600

    Added color maps root setting
    Necessary to build a color map file for use in 2-bit PNG image conversion.
    
    Milestone: minor

--- config/settings.rb
+++ config/settings.rb
@@ -10,6 +10,10 @@ module Terminus
             constructor: Types::Params::String,
             default: "http://#{IPFinder.new.wired}:2300"
 
+    setting :color_maps_root,
+            constructor: Terminus::Types::Pathname,
+            default: Hanami.app.root.join("public/assets/color_maps")
+
     setting :firmware_poller, constructor: Types::Params::Bool, default: true
     setting :model_poller, constructor: Types::Params::Bool, default: true
     setting :screen_poller, constructor: Types::Params::Bool, default: true
--- spec/support/shared_contexts/application_dependencies.rb
+++ spec/support/shared_contexts/application_dependencies.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 RSpec.shared_context "with application dependencies" do
+  include_context "with temporary directory"
+
   let(:app) { Hanami.app }
   let(:settings) { app[:settings] }
   let(:routes) { app[:routes] }
   let(:json_payload) { JSON last_response.body, symbolize_names: true }
 
-  before { allow(settings).to receive(:api_uri).and_return("https://localhost") }
+  before do
+    allow(settings).to receive_messages(api_uri: "https://localhost", color_maps_root: temp_dir)
+  end
 end
