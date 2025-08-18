01318a027ef7ab671ddb1bcf4993d8078e84b103
Brooke Kuhlmann <brooke@alchemists.io>
2025-08-18 13:32:54 -0600

    Added model struct crop and rotation methods
    Necessary to teach the struct how to supply crop information to MiniMagic but only if the model can be cropped. Similar to cropping, a rotation check is provided now too.
    
    Milestone: minor

--- app/structs/model.rb
+++ app/structs/model.rb
@@ -4,7 +4,13 @@ module Terminus
   module Structs
     # The model struct.
     class Model < DB::Struct
+      def crop = "#{dimensions}+#{offset_x}+#{offset_y}"
+
+      def cropable? = !offset_x.zero? || !offset_y.zero?
+
       def dimensions = "#{width}x#{height}"
+
+      def rotatable? = rotation.positive?
     end
   end
 end
