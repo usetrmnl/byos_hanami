0b646a803a66ce500e9ccb245fe5fb47041b0c92
Brooke Kuhlmann <brooke@alchemists.io>
2025-08-25 19:31:50 -0600

    Added playlist part
    Necessary to render current screen URI (if associated). Unfortunately, ROM Factory can't build nested associations. Worse, you can't stub the associations either. So the spec setup is more complicated than desired but ensures a part is iniitialized with a playlist that has a current item.
    
    All functionality is focused the current item of the playlist which can be a pill that displays current status along with the current screen URI (if present).
    
    Milestone: minor

---
+++ app/views/parts/playlist.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "hanami/view"
+
+module Terminus
+  module Views
+    module Parts
+      # The playlist presenter.
+      class Playlist < Hanami::View::Part
+        include Deps[:assets]
+
+        def current_pill item
+          return unless current_item_id == item.id
+
+          helpers.tag.span "Current", class: "bit-pill bit-active"
+        end
+
+        # :reek:ManualDispatch
+        def current_screen_uri
+          if value.current_item_id && value.respond_to?(:current_item)
+            value.current_item.screen.image_uri
+          else
+            assets["blank.svg"].path
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/app/views/parts/playlist_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Views::Parts::Playlist, :db do
+  subject :part do
+    playlist = Factory[:playlist]
+    screen = Factory[:screen, :with_image]
+    item = Factory[:playlist_item, playlist_id: playlist.id, screen_id: screen.id]
+
+    repository = Hanami.app["repositories.playlist"]
+    repository.update playlist.id, current_item_id: item.id
+
+    described_class.new value: repository.find(playlist.id), rendering: rendering
+  end
+
+  let(:rendering) { view.new.rendering }
+
+  let :view do
+    Class.new Hanami::View do
+      config.paths = [Hanami.app.root.join("app/templates")]
+      config.template = "n/a"
+    end
+  end
+
+  before { allow(rendering).to receive(:context).and_return Terminus::Views::Context.new }
+
+  describe "#current_pill" do
+    it "answers current pill when current item, screen, and image exist" do
+      expect(part.current_pill(part.current_item)).to eq(
+        %(<span class="bit-pill bit-active">Current</span>)
+      )
+    end
+
+    it "answers nil when current item is missing" do
+      part = described_class.new value: Factory[:playlist], rendering: view.new.rendering
+      item = Factory[:playlist_item]
+
+      expect(part.current_pill(item)).to be(nil)
+    end
+  end
+
+  describe "#current_screen_uri" do
+    it "answers URI when current item, screen, and image exist" do
+      expect(part.current_screen_uri).to eq("memory://abc123.png")
+    end
+
+    it "answers fallback when current item is missing" do
+      part = described_class.new value: Factory[:playlist], rendering: view.new.rendering
+      expect(part.current_screen_uri).to match(%r(/assets/blank.*\.svg))
+    end
+  end
+end
