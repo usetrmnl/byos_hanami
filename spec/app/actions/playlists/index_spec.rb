a2a631450bd2d83aa09e54194d8e9137d8ad3a47
Brooke Kuhlmann <brooke@alchemists.io>
2025-08-25 19:34:11 -0600

    Updated playlist template to include the rendering of current screen
    Necessary to see the current screen being displayed when managing playlists.
    
    Milestone: minor

--- spec/app/actions/playlists/index_spec.rb
+++ spec/app/actions/playlists/index_spec.rb
@@ -6,7 +6,15 @@ RSpec.describe Terminus::Actions::Playlists::Index, :db do
   subject(:action) { described_class.new }
 
   describe "#call" do
-    let(:playlist) { Factory[:playlist, label: "Test", name: "test"] }
+    let :playlist do
+      playlist = Factory[:playlist, label: "Test", name: "test"]
+      screen = Factory[:screen, :with_image]
+      item = Factory[:playlist_item, playlist_id: playlist.id, screen_id: screen.id]
+
+      repository = Hanami.app["repositories.playlist"]
+      repository.update playlist.id, current_item_id: item.id
+      repository.find playlist.id
+    end
 
     it "renders standard response with search results" do
       response = Rack::MockRequest.new(action).get "", params: {query: playlist.label}
