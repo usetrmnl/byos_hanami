23b5f95f3ff604597d942edc901119d0ee78fecc
Brooke Kuhlmann <brooke@alchemists.io>
2025-08-28 14:15:55 -0600

    Updated screens color mapper to handle bit depths higher than four
    Necessary to allow higher bit depths by logging a debug message when a bit depth highter than four is given. This'll allow for the various converters to address accordingly by checking if the path exists or not.
    
    RuboCop and Reek have been updated since there breaking down the methods won't help in this case.
    
    Issue: 219
    Milestone: minor

--- app/aspects/screens/color_mapper.rb
+++ app/aspects/screens/color_mapper.rb
@@ -9,6 +9,7 @@ module Terminus
       # Creates a color map for use in PNG image conversion.
       class ColorMapper
         include Deps[:settings]
+        include Dependencies[:logger]
         include Dry::Monads[:result]
         include Initable[base: 2, command: "magick", shell: Open3]
 
@@ -22,8 +23,7 @@ module Terminus
         private
 
         def create bit_depth, dimensions, path
-          colorize(bit_depth).fmap { |colors| build_command_line dimensions, colors }
-                             .bind { |command_line| run command_line, path }
+          colorize(bit_depth).bind { |colors| run colors, dimensions, path }
         end
 
         def colorize bit_depth
@@ -31,20 +31,30 @@ module Terminus
             when 1 then Success %w[xc:#000 xc:#FFF]
             when 2 then Success %w[xc:#000 xc:#555 xc:#AAA xc:#FFF]
             when 4 then Success((0..15).map { "xc:##{it.to_s(16).upcase * 3}" })
-            else Failure "Unable to create grayscale color map for bit depth: #{bit_depth}."
+            else debug { "Color map skipped for bit depth: #{bit_depth}." }
           end
         end
 
-        def build_command_line dimensions, colors
-          [command, "-size", dimensions, *colors, "+append", "-type", "Palette"]
-        end
+        def run colors, dimensions, path
+          return Success(Pathname.new("")) unless colors
 
-        def run command_line, path
-          shell.capture3(*command_line, path.to_s).then do |_output, error, status|
-            status.success? ? Success(path) : Failure(error)
-          end
+          *, error, status = shell.capture3 command,
+                                            "-size",
+                                            dimensions,
+                                            *colors,
+                                            "+append",
+                                            "-type",
+                                            "Palette",
+                                            path.to_s
+
+          status.success? ? Success(path) : Failure(error)
         rescue Errno::ENOENT => error
           Failure error.message
+        end
+
+        def debug(&)
+          logger.debug(&)
+          Success nil
         end
       end
     end
