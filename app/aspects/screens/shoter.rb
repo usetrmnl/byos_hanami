0eab22648325430de29cc34fc44949735f26ddd9
Brooke Kuhlmann <brooke@alchemists.io>
2025-09-02 15:56:47 -0600

    Added screen shoter error handling
    Necessary to bubble up more specific problem details when unable to capture a screenshot for further processing. Debug logging is also used to capture any/all issues further investigation if desired.
    
    Reek and RuboCop has been updated to ignore all of the error handling since there isn't a great way to break this up further. Keeping all of this error logic together helps understand all of the different errors that could arise.
    
    Issue: 221
    Milestone: minor

--- app/aspects/screens/shoter.rb
+++ app/aspects/screens/shoter.rb
@@ -9,6 +9,7 @@ module Terminus
     module Screens
       # Saves web page as screenshot.
       class Shoter
+        include Dependencies[:logger]
         include Dry::Monads[:result]
 
         using Refinements::Pathname
@@ -25,24 +26,22 @@ module Terminus
 
         VIEWPORT = {width: 800, height: 480, scale_factor: 1}.freeze
 
-        def initialize settings: SETTINGS, browser: Ferrum::Browser
+        def initialize(settings: SETTINGS, browser: Ferrum::Browser, **)
           @settings = settings
           @browser = browser
+          super(**)
         end
 
-        def call content, output_path, viewport: VIEWPORT
-          save content, viewport, output_path
-          Success output_path
-        end
+        def call(content, output_path, viewport: VIEWPORT) = save content, viewport, output_path
 
         private
 
         attr_reader :settings, :browser
 
         def save content, viewport, output_path
-          Pathname.mktmpdir do |work_dir|
-            instance = browser.new settings
+          instance = browser.new settings
 
+          Pathname.mktmpdir do |work_dir|
             instance.create_page
             instance.set_viewport(**viewport)
             instance.main_frame.content = work_dir.join("content.html").write(content).read
@@ -50,6 +49,53 @@ module Terminus
             instance.screenshot path: output_path.to_s
             instance.quit
           end
+
+          Success output_path
+        rescue Ferrum::BrowserError => error then handle_browser_error instance, error
+        rescue Ferrum::DeadBrowserError => error then handle_dead_browser_error instance, error
+        rescue Ferrum::TimeoutError => error then handle_timeout_error instance, error
+        rescue Ferrum::NoSuchTargetError => error then handle_no_such_target_error instance, error
+        rescue Ferrum::ProcessTimeoutError => error
+          handle_process_timeout_error instance, error
+        end
+
+        def handle_browser_error instance, error
+          instance.quit
+          logger.debug { "Screen shoter has browser error: #{error.message}" }
+
+          Failure "Unable to capture screenshot due to an instance error such as " \
+                  "page navigation, element interaction, or something else."
+        end
+
+        def handle_dead_browser_error instance, error
+          instance.quit
+          logger.debug { "Screen shoter has dead browser: #{error.message}" }
+
+          Failure "Unable to capture screenshot due to a dead browser. " \
+                  "This could mean the browser crashed, server is out of memory, " \
+                  "or a resource limitation has been hit."
+        end
+
+        def handle_timeout_error instance, error
+          instance.quit
+          logger.debug { "Screen shoter has timeout: #{error.message}" }
+
+          Failure "Unable to capture screenshot due to timming out while waiting for response. " \
+                  "This might have happened due to the page taking a long time to load."
+        end
+
+        def handle_no_such_target_error instance, error
+          instance.quit
+          logger.debug { "Screen shoter has no such target: #{error.message}" }
+          Failure "Unable to capture screenshot because the page closed or crashed."
+        end
+
+        def handle_process_timeout_error instance, error
+          instance.quit
+          logger.debug { "Screen shoter has process timeout: #{error.message}" }
+
+          Failure "Unable to capture screenshot because the browser could not produce a " \
+                  "websocket URL within the expected amount of time."
         end
       end
     end
--- spec/app/aspects/screens/shoter_spec.rb
+++ spec/app/aspects/screens/shoter_spec.rb
@@ -4,9 +4,10 @@ require "hanami_helper"
 require "mini_magick"
 
 RSpec.describe Terminus::Aspects::Screens::Shoter do
-  subject(:screensaver) { described_class.new }
+  subject(:shoter) { described_class.new }
 
   include_context "with temporary directory"
+  include_context "with library dependencies"
 
   describe "#call" do
     let :content do
@@ -27,16 +28,141 @@ RSpec.describe Terminus::Aspects::Screens::Shoter do
     end
 
     let(:path) { temp_dir.join "test.jpeg" }
+    let(:instance) { instance_spy Ferrum::Browser }
+    let(:browser) { class_double Ferrum::Browser, new: instance }
 
     it "creates screenshot" do
-      screensaver.call content, path
+      shoter.call content, path
       image = MiniMagick::Image.open path
 
       expect(image).to have_attributes(width: 800, height: 480, type: "JPEG", exif: {})
     end
 
     it "answers image path" do
-      expect(screensaver.call(content, path)).to be_success(path)
+      expect(shoter.call(content, path)).to be_success(path)
+    end
+
+    context "with browser error" do
+      subject(:shoter) { described_class.new browser: }
+
+      before { allow(instance).to receive(:create_page).and_raise(Ferrum::BrowserError, "Danger!") }
+
+      it "quits browser instance" do
+        shoter.call content, path
+        expect(instance).to have_received(:quit)
+      end
+
+      it "logs debug message" do
+        shoter.call content, path
+        expect(logger.reread).to match(/DEBUG.+Screen shoter has browser error.+/)
+      end
+
+      it "answers failure" do
+        expect(shoter.call(content, path)).to be_failure(
+          "Unable to capture screenshot due to an instance error such as " \
+          "page navigation, element interaction, or something else."
+        )
+      end
+    end
+
+    context "with dead browser error" do
+      subject(:shoter) { described_class.new browser: }
+
+      before do
+        allow(instance).to receive(:create_page).and_raise(Ferrum::DeadBrowserError, "Danger!")
+      end
+
+      it "quits browser instance" do
+        shoter.call content, path
+        expect(instance).to have_received(:quit)
+      end
+
+      it "logs debug message" do
+        shoter.call content, path
+        expect(logger.reread).to match(/DEBUG.+Screen shoter has dead browser: Danger!/)
+      end
+
+      it "answers failure" do
+        expect(shoter.call(content, path)).to be_failure(
+          "Unable to capture screenshot due to a dead browser. " \
+          "This could mean the browser crashed, server is out of memory, " \
+          "or a resource limitation has been hit."
+        )
+      end
+    end
+
+    context "with timeout error" do
+      subject(:shoter) { described_class.new browser: }
+
+      before { allow(instance).to receive(:create_page).and_raise(Ferrum::TimeoutError) }
+
+      it "answers failure" do
+        expect(shoter.call(content, path)).to be_failure(
+          "Unable to capture screenshot due to timming out while waiting for response. " \
+          "This might have happened due to the page taking a long time to load."
+        )
+      end
+
+      it "logs debug message" do
+        shoter.call content, path
+        expect(logger.reread).to match(/DEBUG.+Screen shoter has timeout.+/)
+      end
+
+      it "quits browser instance" do
+        shoter.call content, path
+        expect(instance).to have_received(:quit)
+      end
+    end
+
+    context "with no such target error" do
+      subject(:shoter) { described_class.new browser: }
+
+      before do
+        allow(instance).to receive(:create_page).and_raise(Ferrum::NoSuchTargetError, "Danger!")
+      end
+
+      it "quits browser instance" do
+        shoter.call content, path
+        expect(instance).to have_received(:quit)
+      end
+
+      it "logs debug message" do
+        shoter.call content, path
+        expect(logger.reread).to match(/DEBUG.+Screen shoter has no such target: Danger!/)
+      end
+
+      it "answers failure" do
+        expect(shoter.call(content, path)).to be_failure(
+          "Unable to capture screenshot because the page closed or crashed."
+        )
+      end
+    end
+
+    context "with processing timeout error" do
+      subject(:shoter) { described_class.new browser: }
+
+      before do
+        allow(instance).to receive(:create_page).and_raise(
+          Ferrum::ProcessTimeoutError.new(1, "n/a")
+        )
+      end
+
+      it "quits browser instance" do
+        shoter.call content, path
+        expect(instance).to have_received(:quit)
+      end
+
+      it "logs debug message" do
+        shoter.call content, path
+        expect(logger.reread).to match(/DEBUG.+Screen shoter has process timeout.+/)
+      end
+
+      it "answers failure" do
+        expect(shoter.call(content, path)).to be_failure(
+          "Unable to capture screenshot because the browser could not produce a " \
+          "websocket URL within the expected amount of time."
+        )
+      end
     end
   end
 end
