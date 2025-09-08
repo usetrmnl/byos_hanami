7e9d40a90a0035a8ba98dc8030740b457cab7fad
Brooke Kuhlmann <brooke@alchemists.io>
2025-09-08 13:48:46 -0600

    Added application secret setting
    Necessary to start leveraging application session and cookies. This will also be used for authentication purposes.
    
    Milestone: minor

--- config/settings.rb
+++ config/settings.rb
@@ -6,9 +6,8 @@ require "terminus/types"
 module Terminus
   # The application base settings.
   class Settings < Hanami::Settings
-    setting :api_uri,
-            constructor: Types::Params::String,
-            default: "http://#{IPFinder.new.wired}:2300"
+    setting :api_uri, constructor: Types::String, default: "http://#{IPFinder.new.wired}:2300"
+    setting :app_secret, constructor: Types::String.constrained(filled: true)
 
     setting :color_maps_root,
             constructor: Terminus::Types::Pathname,
