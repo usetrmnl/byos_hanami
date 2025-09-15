b15b00d10c3ce8a6ffa71fad3792d05d69dccde8
Brooke Kuhlmann <brooke@alchemists.io>
2025-09-15 16:43:25 -0600

    Added user factory
    Necessary to create users for testing purposes.
    
    Milestone: minor

---
+++ spec/support/factories/user.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+Factory.define :user, relation: :user do |factory|
+  factory.sequence(:email) { "user_#{it}@test.io" }
+  factory.sequence(:name) { "User #{it}" }
+end
