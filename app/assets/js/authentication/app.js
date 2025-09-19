763bc15c484845591116393e03990f2a3d50a736
Brooke Kuhlmann <brooke@alchemists.io>
2025-09-19 16:12:07 -0600

    Added authentication assets
    Necessary to style the authentication portal when logging in, logging out, registering, etc.
    
    Milestone: minor

---
+++ app/assets/js/authentication/app.js
@@ -0,0 +1,6 @@
+import "../../css/settings.css";
+import "../../css/colors.css";
+import "../../css/view_transitions.css";
+import "../../css/defaults.css";
+import "../../css/layout.css";
+import "../../css/pages/authentication.css";
