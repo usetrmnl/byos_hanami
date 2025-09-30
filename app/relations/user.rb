1dac47725137ad357e5744c586a9d0a52cbbb7cb
Brooke Kuhlmann <brooke@alchemists.io>
2025-09-30 14:26:08 -0600

    Added user relation belongs to status association
    Necessary to associate users with corresponding status.
    
    Milestone: minor

--- app/relations/user.rb
+++ app/relations/user.rb
@@ -5,7 +5,10 @@ module Terminus
     # The user relation.
     class User < DB::Relation
       schema :user, infer: true do
-        associations { has_many :memberships, relation: :membership }
+        associations do
+          belongs_to :user_status, relation: :user_status, as: :status
+          has_many :memberships, relation: :membership
+        end
       end
     end
   end
