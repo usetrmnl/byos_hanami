44b449760aea431b0f24344f7de7ce146fbba755
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-10 12:54:49 -0600

    Added Git tag and SHA settings
    Necessary to calculate the latest Git tag and commit SHA information. This will soon be used to render this information within the site footer.
    
    Milestone: minor

--- config/settings.rb
+++ config/settings.rb
@@ -17,6 +17,14 @@ module Terminus
             constructor: Terminus::Types::Pathname,
             default: Hanami.app.root.join("public/assets/color_maps")
 
+    setting :git_tag,
+            constructor: Types::String.constrained(filled: true),
+            default: `git tag --list --sort=taggerdate | tail -n 1`.strip
+
+    setting :git_sha,
+            constructor: Types::String.constrained(filled: true),
+            default: `git rev-parse --short HEAD`.strip
+
     setting :browser, constructor: Terminus::Types::Browser, default: "{}"
     setting :firmware_poller, constructor: Types::Bool, default: true
     setting :model_poller, constructor: Types::Bool, default: true
