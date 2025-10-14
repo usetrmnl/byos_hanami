3626ef3880a634a18d6ef1da7a40e68f86d80285
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-14 11:21:27 -0600

    Added extension migration
    Necessary to manage extension records which are an evolution of what is known as _plugins_ in Core. The design of this table combines Core plugin types such as native, private, public, third party, and recipes. The column names and terminology is simplified and adapted for this architecture in order to avoid the mistakes of the Core system while also being able to scale and support different extensions in the future.
    
    Milestone: minor

---
+++ config/db/migrate/20251111145213_create_extension.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+ROM::SQL.migration do
+  change do
+    create_enum :extension_kind_enum, %w[image poll secure static webhook]
+    create_enum :extension_mode_enum, %w[dark light]
+    create_enum :extension_verb_enum, %w[get post]
+    create_enum :extension_repeat_type_enum, %w[none minute hour day week month]
+
+    create_table :extension do
+      primary_key :id, type: :Bignum
+
+      column :name, String, unique: true, index: true, null: false
+      column :label, String, unique: true, null: false
+      column :logo_data, :jsonb, null: false, default: "{}"
+      column :description, :text
+      column :kind, :extension_kind_enum, null: false, default: "poll"
+      column :mode, :extension_mode_enum, null: false, default: "light"
+      column :tags, "text[]", null: false, default: "{}"
+      column :headers, :jsonb, null: false, default: "{}"
+      column :verb, :extension_verb_enum, null: false, default: "get"
+      column :uris, "text[]", null: false, default: "{}"
+      column :body, :jsonb, null: false, default: "{}"
+      column :fields, :jsonb, null: false, default: "{}"
+      column :template, :text
+      column :globals, :jsonb, null: false, default: "{}"
+      column :repeat_interval, Integer, null: false, default: 1
+      column :repeat_type, :extension_repeat_type_enum, index: true, null: false, default: "none"
+      column :repeat_days, "text[]", null: false, default: "{}"
+      column :last_day_of_month, :boolean, null: false, default: false
+      column :created_at, :timestamp, null: false, default: Sequel::CURRENT_TIMESTAMP
+      column :updated_at, :timestamp, null: false, default: Sequel::CURRENT_TIMESTAMP
+    end
+  end
+end
