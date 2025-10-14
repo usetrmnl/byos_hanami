8a60eb7526d81145ef05ccb1159263ef9bda12f5
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-14 13:16:01 -0600

    Added extensions show resource
    Necessary to render a single extension.
    
    Milestone: minor

---
+++ app/views/extensions/show.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Views
+    module Extensions
+      # The show view.
+      class Show < Terminus::View
+        expose :extension
+      end
+    end
+  end
+end
