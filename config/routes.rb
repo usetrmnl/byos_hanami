52de97ca4573222280e5e5b68d9804f6e4897b46
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-14 13:20:01 -0600

    Added extensions new resource
    Necessary to create a new extension. The fields will also be shared with create, edit, and update actions.
    
    Milestone: minor

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,7 @@ module Terminus
 
     get "/extensions", to: "extensions.index", as: :extensions
     get "/extensions/:id", to: "extensions.show", as: :extension
+    get "/extensions/new", to: "extensions.new", as: :extension_new
 
     get "/firmware", to: "firmware.index", as: :firmware
     delete "/firmware/:id", to: "firmware.delete", as: :firmware_delete
