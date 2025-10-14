0ed587aaf4c4eb61571103b0f5c8c4c9a2d78566
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-14 11:21:27 -0600

    Added extension migration
    Necessary to manage extension records which are an evolution of what is known as _plugins_ in Core. The design of this table combines Core plugin types such as native, private, public, third party, and recipes. The column names and terminology is simplified and adapted for this architecture in order to avoid the mistakes of the Core system while also being able to scale and support different extensions in the future.
    
    Milestone: minor

---
+++ config/db/migrate/20251014103858_create_extension.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+ROM::SQL.migration do
+  change do
+    create_enum :extension_kind_enum, %w[image poll secure static webhook]
+    create_enum :extension_mode_enum, %w[dark light]
+    create_enum :extension_verb_enum, %w[get post]
+    create_enum :extension_repeat_type_enum, %w[none minute hour day week month year]
+
+    create_table :extension do
+      primary_key :id, type: :Bignum
+
+      column :name, String, index: true, null: false
+      column :label, String, null: false
+      column :logo_data, :jsonb, null: false, default: "{}"
+      column :description, :text
+      column :kind, :extension_kind_enum, null: false, default: "poll"
+      column :mode, :extension_mode_enum, null: false, default: "light"
+      column :tags, "text[]", null: false, default: "{}"
+      column :headers, :jsonb, null: false, default: "{}"
+      column :verb, :extension_verb_enum, null: false, default: "get"
+      column :uri, String
+      column :poll_body, :jsonb, null: false, default: "{}"
+      column :static_body, :jsonb, null: false, default: "{}"
+      column :fields, :jsonb, null: false, default: "{}"
+      column :padding, :boolean, null: false, default: false
+      column :template_full, :text
+      column :template_horizontal, :text
+      column :template_vertical, :text
+      column :template_quarter, :text
+      column :template_shared, :text
+      column :repeat_interval, Integer, null: false, default: 1
+      column :repeat_type, :extension_repeat_type_enum, index: true, null: false, default: "day"
+      column :repeat_days, "text[]", null: false, default: "{}"
+      column :last_day_of_month, :boolean, null: false, default: false
+      column :created_at, :timestamp, null: false, default: Sequel::CURRENT_TIMESTAMP
+      column :updated_at, :timestamp, null: false, default: Sequel::CURRENT_TIMESTAMP
+    end
+  end
+end
