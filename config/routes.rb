d73ad163ddf6559dfae5136748c0cd551f2ae0bc
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-14 13:24:16 -0600

    Added extensions delete action
    Necessary to delete an existing action.
    
    Milestone: minor

--- config/routes.rb
+++ config/routes.rb
@@ -66,6 +66,7 @@ module Terminus
     post "/extensions", to: "extensions.create", as: :extension_create
     get "/extensions/:id/edit", to: "extensions.edit", as: :extension_edit
     put "/extensions/:id", to: "extensions.update", as: :extension_update
+    delete "/extensions/:id", to: "extensions.delete", as: :extension_delete
 
     get "/firmware", to: "firmware.index", as: :firmware
     delete "/firmware/:id", to: "firmware.delete", as: :firmware_delete
