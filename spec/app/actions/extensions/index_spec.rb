3ab8e5f8502e5f3db9091ffb6b429ce4b1a76617
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-14 13:14:05 -0600

    Added extensions index resource
    Necessary to render the index view of extensions.
    
    Milestone: minor

---
+++ spec/app/actions/extensions/index_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Actions::Extensions::Index, :db do
+  subject(:action) { described_class.new }
+
+  describe "#call" do
+    let(:extension) { Factory[:extension, label: "Test", name: "test"] }
+
+    it "renders standard response with search results" do
+      response = Rack::MockRequest.new(action).get "", params: {query: extension.label}
+      expect(response.body).to include(%(<h2 class="label">Test</h2>))
+    end
+
+    it "renders standard response with no results" do
+      response = Rack::MockRequest.new(action).get "", params: {query: "bogus"}
+      expect(response.body).to include("No extensions found.")
+    end
+
+    it "renders htmx response with search results" do
+      response = Rack::MockRequest.new(action).get "",
+                                                   "HTTP_HX_TRIGGER" => "search",
+                                                   params: {query: extension.label}
+
+      expect(response.body).to include(%(<h2 class="label">Test</h2>))
+    end
+
+    it "renders htmx response with no results" do
+      response = Rack::MockRequest.new(action).get "",
+                                                   "HTTP_HX_TRIGGER" => "search",
+                                                   params: {query: "bogus"}
+
+      expect(response.body).to include("No extensions found.")
+    end
+
+    it "renders all extensions with no query" do
+      extension
+      response = Rack::MockRequest.new(action).get "", "HTTP_HX_TRIGGER" => "search"
+
+      expect(response.body).to include(%(<h2 class="label">Test</h2>))
+    end
+  end
+end
