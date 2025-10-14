849d3704493cb3fa5c812e7379196613ccf0f7d2
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-14 11:38:11 -0600

    Added extension factory
    Necessary for testing purposes in order to create an extension record with safe defaults.
    
    Milestone: minor

---
+++ spec/support/factories/extension.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+Factory.define :extension, relation: :extension do |factory|
+  factory.sequence(:name) { "extension_#{it}" }
+  factory.sequence(:label) { "Extension #{it}" }
+  factory.kind "poll"
+  factory.mode "light"
+  factory.verb "get"
+  factory.uris []
+  factory.repeat_type "none"
+
+  factory.trait :with_logo do |trait|
+    trait.logo_data do
+      {
+        id: "logo.png",
+        storage: "store",
+        metadata: {
+          bit_depth: 16,
+          filename: "logo.png",
+          height: 512,
+          mime_type: "image/png",
+          size: 50,
+          width: 512
+        }
+      }
+    end
+  end
+end
