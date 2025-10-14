d2655e55fb3caf8919f24aa305df56a5de7fd89d
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-14 13:16:01 -0600

    Added extensions show resource
    Necessary to render a single extension.
    
    Milestone: minor

---
+++ spec/app/actions/extensions/show_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Actions::Extensions::Show, :db do
+  subject(:action) { described_class.new }
+
+  describe "#call" do
+    let(:extension) { Factory[:extension] }
+
+    it "renders default response" do
+      response = Rack::MockRequest.new(action).get "", params: {id: extension.id}
+      expect(response.body).to include("<!DOCTYPE html>")
+    end
+
+    it "renders htmx response" do
+      response = Rack::MockRequest.new(action)
+                                  .get "", "HTTP_HX_REQUEST" => "true", params: {id: extension.id}
+
+      expect(response.body).to have_htmx_title("#{extension.label} Extension")
+    end
+
+    it "answers unprocessable entity with invalid parameters" do
+      response = action.call Hash.new
+      expect(response.status).to eq(422)
+    end
+  end
+end
