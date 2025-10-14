a6f7b69cde7029177533dfd5b2bd1aeae3dbf8d9
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-14 13:23:42 -0600

    Added extensions update action
    Necessary to update an existing extension.
    
    Milestone: minor

---
+++ app/actions/extensions/update.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Actions
+    module Extensions
+      # The update action.
+      class Update < Terminus::Action
+        include Deps[repository: "repositories.extension", show_view: "views.extensions.show"]
+
+        params do
+          required(:id).filled :integer
+
+          required(:extension).filled(:hash) do
+            required(:name).filled :string
+            required(:label).filled :string
+            required(:description).maybe :string
+            required(:kind).filled :string
+          end
+        end
+
+        def handle request, response
+          parameters = request.params
+          extension = repository.find parameters[:id]
+
+          halt :unprocessable_entity unless extension
+
+          if parameters.valid?
+            save extension, parameters, response
+          else
+            error extension, parameters, response
+          end
+        end
+
+        private
+
+        def save extension, parameters, response
+          id = extension.id
+          repository.update id, **parameters[:extension]
+
+          response.render show_view, extension: repository.find(id), layout: false
+        end
+
+        # :reek:FeatureEnvy
+        def error extension, parameters, response
+          response.render view,
+                          extension:,
+                          fields: parameters[:extension],
+                          errors: parameters.errors[:extension],
+                          layout: false
+        end
+      end
+    end
+  end
+end
