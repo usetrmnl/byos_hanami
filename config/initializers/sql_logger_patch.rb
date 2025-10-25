741096cb582b9699049a342addd8fe5b26009677
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-25 08:53:44 -0600

    Added SQL logger patch initializer
    Necessary to ensure our custom logger provider (i.e. Cogger) is properly picked up and used for all database logging. Unfortunately, this monkey patches Dry Monitor's SQL logger which is a necessary evil. This is especially problematic when we've provided our own custom logger provider which Hanami knows nothing about. Due to this situation, this initializer is used to monkey patch the Dry Monitor SQL Logger so our logger is picked up along with proper tagging for all database queries.
    
    Milestone: minor

---
+++ config/initializers/sql_logger_patch.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "dry-monitor"
+require "dry/monitor/sql/logger"
+
+# Patches Hanami's default SQL logger.
+module SQLLoggerPatch
+  # :reek:UtilityFunction
+  def log_query time:, name:, query:
+    Hanami.app[:logger].info { {message: query, tags: [{db: name, duration: time}]} }
+  end
+end
+
+Dry::Monitor::SQL::Logger.prepend SQLLoggerPatch
