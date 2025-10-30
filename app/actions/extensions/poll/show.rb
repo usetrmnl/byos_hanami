7e3fa66f0e41ab981d09125f8a6f26606d39cba3
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-30 11:43:21 -0600

    Added extensions poll show action
    Necessary to load data for actions that have poll data.
    
    Milestone: minor

---
+++ app/actions/extensions/poll/show.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "dry/monads"
+require "initable"
+
+module Terminus
+  module Actions
+    module Extensions
+      module Poll
+        # The show action.
+        class Show < Terminus::Action
+          include Deps[
+            repository: "repositories.extension",
+            fetcher: "aspects.extensions.multi_fetcher"
+          ]
+          include Dry::Monads[:result]
+
+          params { required(:id).filled :integer }
+
+          def handle request, response
+            parameters = request.params
+
+            halt :unprocessable_entity unless parameters.valid?
+
+            extension = repository.find parameters[:id]
+
+            halt :not_found unless extension
+
+            response.render view, body: body_for(extension), layout: false
+          end
+
+          private
+
+          def body_for extension
+            case fetcher.call extension
+              in Success(body) then body
+              in Failure(message) then message
+              else "Unable to render body for extension: #{extension.id}."
+            end
+          end
+        end
+      end
+    end
+  end
+end
