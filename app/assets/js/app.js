3cf1580f88153889ef87678bdefda9bb12b82944
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-30 14:45:00 -0600

    Added CodeMirror
    Necessary to provide syntax highlighting when editing source code. Supports the following features:
    
    * Allows any HTML element to be turned into editable content by adding the `bit-editor` class.
    * Adds custom styling via the `cm-editor` class which is dynamically added to DOM elements when CodeMirror renders the editor.
    * Uses the One Dark Theme for a black console look and feel.
    * Provides JSON and Liquid syntax highlighting (might need to add more in the future but is good for now).
    * Provides dynamic language support via the HTML `data-language` attribute. Use: `json` or `liquid` as desired.
    * Provides dynamic read or write mode. The default is read/write but if you want the editor to be read only, use the `data-mode="read"` HTML attribute.
    * The `EditorView` is configured to dynamically listen for updates to the HTML element which has the `.bit-editor` class. All updates are kept in sync so that when the form is submitted, all changes are saved. The original element (usually a form field) is hidden from view while the editor is loaded.
    * Provides tab and selection indentation key maps.
    * Uses the `data-initialized` attribute to keep track of whether the editor is initialized or not. When `true` that means initialization can be skipped. This is especially important then used in conjunction with htmx partial updates.
    
    See project link:https://codemirror.net[documentation] for further details.
    
    Milestone: minor

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -9,6 +9,7 @@ import "../css/bits/buttons.css";
 import "../css/bits/cards.css";
 import "../css/bits/containers.css";
 import "../css/bits/definition_lists.css";
+import "../css/bits/editor.css";
 import "../css/bits/empty.css";
 import "../css/bits/forms.css";
 import "../css/bits/headers.css";
@@ -35,3 +36,63 @@ Alpine.start();
 
 import "htmx-ext-sse";
 import "htmx-remove";
+
+import { basicSetup } from "codemirror";
+import { EditorView, keymap } from "@codemirror/view";
+import { EditorState } from "@codemirror/state";
+import { oneDark } from "@codemirror/theme-one-dark";
+import { indentWithTab, indentSelection } from "@codemirror/commands";
+import { json } from "@codemirror/lang-json";
+import { liquid } from "@codemirror/lang-liquid";
+
+function initializeCodeMirror() {
+  const languages = {
+    json: json(),
+    liquid: liquid()
+  };
+
+  document.querySelectorAll(".bit-editor").forEach((element) => {
+    if (element.dataset.initialized) return;
+
+    let code = element.value;
+    let language = element.dataset.language;
+    let readable = element.dataset.mode === "read";
+    let container = document.createElement("div");
+    let extensions = [
+      basicSetup,
+      oneDark,
+      keymap.of([indentWithTab, indentSelection]),
+      EditorState.readOnly.of(readable),
+      EditorView.lineWrapping,
+      EditorView.theme({
+        "&": {minHeight: "10rem", maxHeight: "25rem"},
+        ".cm-scroller": {overflow: "auto"}
+      }),
+      EditorView.updateListener.of((update) => {
+        if (update.docChanged) {
+          element.value = update.state.doc.toString();
+        }
+      })
+    ];
+
+    if (languages[language]) {
+      extensions.push(languages[language]);
+    }
+
+    container.className = "cm-container";
+    element.style.display = "none";
+    element.parentNode.insertBefore(container, element.nextSibling);
+
+    new EditorView({
+      doc: code,
+      extensions: extensions,
+      parent: container
+    });
+
+    element.dataset.initialized = "true";
+  });
+}
+
+document.body.addEventListener("htmx:load", function(event) {
+  initializeCodeMirror();
+});
