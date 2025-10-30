58532a4c2348bf72952aac9d047e4e6810631507
Brooke Kuhlmann <brooke@alchemists.io>
2025-10-30 14:45:00 -0600

    Added CodeMirror
    Necessary to provide syntax highlighting when editing source code. link:https://codemirror.net[Documentation].
    
    Milestone: minor

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -9,6 +9,7 @@ import "../css/bits/buttons.css";
 import "../css/bits/cards.css";
 import "../css/bits/containers.css";
 import "../css/bits/definition_lists.css";
+import "../css/bits/editor.css";
 import "../css/bits/empty.css";
 import "../css/bits/forms.css";
 import "../css/bits/headers.css";
@@ -35,3 +36,53 @@ Alpine.start();
 
 import "htmx-ext-sse";
 import "htmx-remove";
+
+import { basicSetup } from "codemirror";
+import { EditorView, keymap } from "@codemirror/view";
+import { indentWithTab } from "@codemirror/commands";
+import { linter, lintGutter } from "@codemirror/lint";
+import { json } from "@codemirror/lang-json";
+import { html } from "@codemirror/lang-html";
+
+function initializeCodeMirror() {
+  const languages = {
+    json: json(),
+    liquid: html()
+  };
+
+  document.querySelectorAll(".bit-editor").forEach((element) => {
+    if (element.dataset.initialized) return;
+
+    let code = element.value;
+    let language = element.dataset.language;
+    let editor = document.createElement("div");
+    let extensions = [
+      basicSetup,
+      keymap.of([indentWithTab]),
+      EditorView.updateListener.of((update) => {
+        if (update.docChanged) {
+          element.value = update.state.doc.toString();
+        }
+      })
+    ];
+
+    if (languages[language]) {
+      extensions.push(languages[language]);
+    }
+
+    element.style.display = "none";
+    element.parentNode.insertBefore(editor, element.nextSibling);
+
+    new EditorView({
+      doc: code,
+      extensions: extensions,
+      parent: editor
+    });
+
+    element.dataset.initialized = "true";
+  });
+}
+
+document.body.addEventListener("htmx:load", function(event) {
+  initializeCodeMirror();
+});
