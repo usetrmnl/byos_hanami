708eea5361a25d043d6bad0d8b758b3f98c2234c
Brooke Kuhlmann <brooke@alchemists.io>
2025-11-03 08:17:07 -0700

    Fixed application setting types
    Necessary to ensure the various pollers can be disabled using falsey values. This cropped up due to being too aggressive in 95db7f1d3e38 (Fixed browser setting defaults) where I dropped the use of `Types::Params` in favor of `Types` only which was incorrect. This fixes that and makes all settings consistent again.
    
    Issue: 236
    Milestone: patch

--- config/settings.rb
+++ config/settings.rb
@@ -6,11 +6,14 @@ require "terminus/types"
 module Terminus
   # The application base settings.
   class Settings < Hanami::Settings
-    setting :api_uri, constructor: Types::String, default: "http://#{IPFinder.new.wired}:2300"
+    setting :api_uri,
+            constructor: Types::Params::String,
+            default: "http://#{IPFinder.new.wired}:2300"
+
     setting :api_access_token_period, constructor: Types::Params::Integer, default: 1_800
 
     setting :app_secret,
-            constructor: Types::String.constrained(filled: true),
+            constructor: Types::Params::String.constrained(filled: true),
             default: SecureRandom.hex(40)
 
     setting :color_maps_root,
@@ -18,16 +21,16 @@ module Terminus
             default: Hanami.app.root.join("public/assets/color_maps")
 
     setting :git_tag,
-            constructor: Types::String.constrained(filled: true),
+            constructor: Types::Params::String.constrained(filled: true),
             default: `git tag --list --sort=taggerdate | tail -n 1`.strip
 
     setting :git_sha,
-            constructor: Types::String.constrained(filled: true),
+            constructor: Types::Params::String.constrained(filled: true),
             default: `git rev-parse --short HEAD`.strip
 
     setting :browser, constructor: Terminus::Types::Browser, default: "{}"
-    setting :firmware_poller, constructor: Types::Bool, default: true
-    setting :model_poller, constructor: Types::Bool, default: true
-    setting :screen_poller, constructor: Types::Bool, default: true
+    setting :firmware_poller, constructor: Types::Params::Bool, default: true
+    setting :model_poller, constructor: Types::Params::Bool, default: true
+    setting :screen_poller, constructor: Types::Params::Bool, default: true
   end
 end
