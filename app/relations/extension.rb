a052aa914a5bb4659a9dabb49ac15680a0175860
Brooke Kuhlmann <brooke@alchemists.io>
2025-11-06 09:33:15 -0700

    Added extension and model relation associations
    Necessary to acquire many models or extensions through a join.
    
    Milestone: minor

--- app/relations/extension.rb
+++ app/relations/extension.rb
@@ -4,7 +4,12 @@ module Terminus
   module Relations
     # The extension relation.
     class Extension < DB::Relation
-      schema :extension, infer: true
+      schema :extension, infer: true do
+        associations do
+          has_many :extension_models, relation: :extension_model
+          has_many :models, through: :extension_model, relation: :model, as: :models
+        end
+      end
     end
   end
 end
