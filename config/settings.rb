52e046e9ea6669b691038f4649181f768d8fc694
Brooke Kuhlmann <brooke@alchemists.io>
2025-11-10 10:15:35 -0700

    Removed default API URI setting
    No longer needed now that this is calculated via the project setup script.
    
    Milestone: patch

--- config/settings.rb
+++ config/settings.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
-require "terminus/ip_finder"
 require "terminus/types"
 
 module Terminus
   # The application base settings.
   class Settings < Hanami::Settings
-    setting :api_uri,
-            constructor: Types::Params::String,
-            default: "http://#{IPFinder.new.wired}:2300"
-
+    setting :api_uri, constructor: Types::Params::String
     setting :api_access_token_period, constructor: Types::Params::Integer, default: 1_800
 
     setting :app_secret,
