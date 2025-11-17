cc70b49451b47ed96ca94df0a0e8451c9830140e
Brooke Kuhlmann <brooke@alchemists.io>
2025-11-17 14:30:26 -0700

    Added extension batch job
    Necessary for enqueuing a job for each model ID associated with the extension.
    
    Milestone: minor

---
+++ app/jobs/batches/extension.rb
@@ -0,0 +1,25 @@
+# auto_register: false
+# frozen_string_literal: true
+
+require "initable"
+
+module Terminus
+  module Jobs
+    module Batches
+      # Enqueues a job for each model ID.
+      class Extension < Base
+        include Deps[repository: "repositories.extension"]
+        include Initable[job: Jobs::ExtensionScreen]
+
+        def perform id
+          extension = repository.find id
+
+          return Failure "Unable to enqueue jobs for extension: #{id}." unless extension
+
+          extension.models.each { |model| job.perform_async extension.id, model.id }
+          Success "Enqueued jobs for extension: #{id}."
+        end
+      end
+    end
+  end
+end
