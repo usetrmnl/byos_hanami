b72842b61f91dddbb8ff888fa63d91aeb6b917ea
Anthony Juckel <ajuckel@gmail.com>
2025-11-27 08:14:55 -0600

    Added canvas to allowed elements
    Enables more rendering options for extensions, e.g. Chart.js.
    
    Milestone: minor

--- app/aspects/sanitizer.rb
+++ app/aspects/sanitizer.rb
@@ -22,10 +22,13 @@ module Terminus
 
       def configuration = client::Config.merge(defaults, elements:, attributes:)
 
-      def elements = defaults[:elements].including "html", "link", "script", "source", "style"
+      def elements
+        defaults[:elements].including "canvas", "html", "link", "script", "source", "style"
+      end
 
       def attributes
-        defaults[:attributes].merge "div" => [:data],
+        defaults[:attributes].merge "canvas" => %w[id width height],
+                                    "div" => [:data],
                                     "link" => %w[href rel],
                                     "script" => %w[src],
                                     "source" => %w[type src srcset sizes media height width]
