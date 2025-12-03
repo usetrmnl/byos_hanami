3fb22086a5c40049f22b3e460f04c74c20cced47
Brooke Kuhlmann <brooke@alchemists.io>
2025-12-03 14:58:49 -0700

    Added playlist relation associations for items and screens
    Necessary to provide access to associated items and screens.
    
    Milestone: minor

--- app/relations/playlist.rb
+++ app/relations/playlist.rb
@@ -8,6 +8,8 @@ module Terminus
         associations do
           belongs_to :current_item, relation: :playlist_item
           has_many :devices, relation: :device
+          has_many :playlist_items, relation: :playlist_item, as: :playlist_items
+          has_many :screens, through: :playlist_item, relation: :screen, as: :screens
         end
       end
     end
