2e6b175474dd1d044bae2be79e65549264e4953c
Brooke Kuhlmann <brooke@alchemists.io>
2025-12-06 10:10:44 -0700

    Added playlist screen view
    Necessary to render an individual screen. Will soon be used by the index and show actions.
    
    Milestone: minor

---
+++ app/views/playlists/screens/show.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "dry/core"
+
+module Terminus
+  module Views
+    module Playlists
+      module Screens
+        # The show view.
+        class Show < Terminus::View
+          expose :playlist
+          expose :before
+          expose :current
+          expose :after
+          expose(:index) { |playlist:, current:| playlist.screens.index current }
+          expose(:total) { |playlist:| playlist.screens.size - 1 }
+          expose(:status) { |index, total| "Slide #{index + 1} of #{total + 1}" if index && total }
+        end
+      end
+    end
+  end
+end
