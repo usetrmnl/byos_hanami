cb9c0a3f15a56c6a6ead2569439102b4d6860740
Brooke Kuhlmann <brooke@alchemists.io>
2025-12-08 14:42:10 -0700

    Added playlist screens feature spec
    Necessary to test that playlist slides can be viewed.
    
    Milestone: minor

--- spec/features/playlists_spec.rb
+++ spec/features/playlists_spec.rb
@@ -46,6 +46,42 @@ RSpec.describe "Playlists", :db do
     expect(page).to have_content("Test II Clone")
   end
 
+  it "plays slides", :aggregate_failures, :js do
+    playlist =  Factory[:playlist]
+
+    (1..3).each do |position|
+      Factory[
+        :playlist_item,
+        playlist_id: playlist.id,
+        screen_id: Factory[:screen, :with_image].id,
+        position:
+      ]
+    end
+
+    visit routes.path(:playlist_screens, playlist_id: playlist.id)
+
+    expect(page).to have_content(playlist.label)
+
+    expect(page).to have_css(%(#progress[aria-label="Slide 1 of 3"]))
+    expect(page).to have_css(%(#progress[value="0"]))
+    expect(page).to have_css(%(#progress[max="2"]))
+
+    click_link "Next"
+
+    expect(page).to have_css(%(#progress[aria-label="Slide 2 of 3"]))
+    expect(page).to have_css(%(#progress[value="1"]))
+
+    click_link "Next"
+
+    expect(page).to have_css(%(#progress[aria-label="Slide 3 of 3"]))
+    expect(page).to have_css(%(#progress[value="2"]))
+
+    click_link "Previous"
+
+    expect(page).to have_css(%(#progress[aria-label="Slide 2 of 3"]))
+    expect(page).to have_css(%(#progress[value="1"]))
+  end
+
   it "deletes playlist", :js do
     playlist = Factory[:playlist]
 
