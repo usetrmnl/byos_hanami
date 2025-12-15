9780362f4f5f55eea791a203e76c6dda2d561287
Brooke Kuhlmann <brooke@alchemists.io>
2025-12-15 16:04:52 -0700

    Added playlist manual and automatic predicates
    Provides a convenient way to determine if the playlist is automatic or manual.
    
    Milestone: minor

--- app/structs/playlist.rb
+++ app/structs/playlist.rb
@@ -4,7 +4,11 @@ module Terminus
   module Structs
     # The playlist struct.
     class Playlist < DB::Struct
+      def automatic? = mode == "automatic"
+
       def current_item_position(default: 1) = current_item ? current_item.position : default
+
+      def manual? = mode == "manual"
     end
   end
 end
--- spec/app/structs/playlist_spec.rb
+++ spec/app/structs/playlist_spec.rb
@@ -5,6 +5,17 @@ require "hanami_helper"
 RSpec.describe Terminus::Structs::Playlist, :db do
   subject(:playlist) { Factory[:playlist] }
 
+  describe "#automatic?" do
+    it "answers true when automatic" do
+      expect(playlist.automatic?).to be(true)
+    end
+
+    it "answers false if not automatic" do
+      playlist = Factory.structs[:playlist, mode: "manual"]
+      expect(playlist.automatic?).to be(false)
+    end
+  end
+
   describe "#current_item_position" do
     let(:update) { Terminus::Repositories::Playlist.new.find playlist.id }
 
@@ -15,6 +26,17 @@ RSpec.describe Terminus::Structs::Playlist, :db do
     it "answers position when current item is available" do
       Factory[:playlist, current_item_id: Factory[:playlist_item, position: 5].id]
       expect(update.current_item_position).to eq(1)
+    end
+  end
+
+  describe "#manual?" do
+    it "answers true when manual" do
+      playlist = Factory.structs[:playlist, mode: "manual"]
+      expect(playlist.manual?).to be(true)
+    end
+
+    it "answers false if not manual" do
+      expect(playlist.manual?).to be(false)
     end
   end
 end
