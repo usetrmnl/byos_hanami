dbbb8cea10d43b023d8f39ad6b9af317b87c730b
Brooke Kuhlmann <brooke@alchemists.io>
2025-12-30 08:41:12 -0700

    Added firmware new resource
    Necessary to manually create new firmware for install on devices. This includes the shared fields that will also be used by the edit action.
    
    Issue: 216
    Milestone: minor

---
+++ app/views/firmware/new.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Views
+    module Firmware
+      # The new view.
+      class New < Terminus::View
+        expose :firmware
+        expose :fields, default: Dry::Core::EMPTY_HASH
+        expose :errors, default: Dry::Core::EMPTY_HASH
+      end
+    end
+  end
+end
