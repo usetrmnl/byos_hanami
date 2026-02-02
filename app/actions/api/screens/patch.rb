9354a4f56c735c690f286b9d949b5ca5d95fc1ff
Brooke Kuhlmann <brooke@alchemists.io>
2026-02-02 16:38:40 -0700

    Updated Screens API patch action to use screen upserter
    Necessary to use the same object when creating screens. This reduces duplication while also enhancing the API.
    
    Issue: 268
    Milestone: minor

--- app/actions/api/screens/patch.rb
+++ app/actions/api/screens/patch.rb
@@ -7,12 +7,8 @@ module Terminus
         # The patch action.
         # :reek:DataClump
         class Patch < Base
-          include Deps[
-            "aspects.screens.upserters.temp_path",
-            repository: "repositories.screen",
-            model_repository: "repositories.model"
-          ]
-          include Initable[mold: Aspects::Screens::Mold, serializer: Serializers::Screen]
+          include Deps["aspects.screens.upserter", repository: "repositories.screen"]
+          include Initable[serializer: Serializers::Screen]
 
           using Refines::Actions::Response
 
@@ -24,66 +20,49 @@ module Terminus
               optional(:label).filled :string
               optional(:name).filled :string
               optional(:content).filled :string
+              optional(:uri).filled :string
+              optional(:data).filled :string
+              optional(:preprocessed).filled :bool
             end
           end
 
           def handle request, response
             parameters = request.params
-            screen = repository.find parameters[:id]
 
-            if parameters.valid? && screen
-              render update(screen, parameters[:screen]), response
+            if parameters.valid?
+              save parameters, response
             else
-              unprocessable_content parameters.errors.to_h, response
+              unprocessable_content_for_parameters parameters.errors.to_h, response
             end
           end
 
           private
 
-          def render result, response
+          def save parameters, response
+            result = find(parameters).bind { |record| update record, parameters }
+
             case result
-              in Success(update) then response.body = {data: serializer.new(update).to_h}.to_json
-              else unprocessable_content_on_failure result, response
+              in Success(screen)
+                response.body = {data: serializer.new(screen).to_h}.to_json
+              else unprocessable_content_for_creation result, response
             end
           end
 
-          def update screen, parameters
-            if parameters.key? :content
-              merge(screen, parameters).bind { |attributes| build_mold attributes }
-                                       .bind { |instance| screenshot instance, screen, parameters }
-            else
-              Success repository.update(screen.id, **parameters)
-            end
+          def find parameters
+            id = parameters[:id]
+            record = repository.find id
+
+            record ? Success(record) : Failure("Unable to find screen: #{id}.")
           end
 
-          def merge screen, parameters
-            Success screen.to_h
-                          .slice(:model_id, :label, :name)
-                          .merge! parameters.slice(:model_id, :label, :name, :content)
+          def update record, parameters
+            upserter.call model_id: record.model_id,
+                          name: record.name,
+                          label: record.label,
+                          **parameters[:screen]
           end
 
-          def build_mold attributes
-            id = attributes[:model_id]
-
-            model_repository.find(id).then do |record|
-              if record
-                Success mold.for(record, **attributes.slice(:label, :name, :content))
-              else
-                Failure "Unable to find model for ID: #{id}."
-              end
-            end
-          end
-
-          def screenshot mold, screen, parameters
-            temp_path.call(mold) { |path| replace path, screen, **parameters }
-          end
-
-          def replace(path, screen, **)
-            path.open { |io| screen.replace io, metadata: {"filename" => path.basename} }
-            Success repository.update(screen.id, image_data: screen.image_attributes, **)
-          end
-
-          def unprocessable_content errors, response
+          def unprocessable_content_for_parameters errors, response
             body = problem[
               type: "/problem_details#screen_payload",
               status: :unprocessable_content,
@@ -95,7 +74,7 @@ module Terminus
             response.with body: body.to_json, format: :problem_details, status: 422
           end
 
-          def unprocessable_content_on_failure result, response
+          def unprocessable_content_for_creation result, response
             body = problem[
               type: "/problem_details#screen_payload",
               status: :unprocessable_content,
