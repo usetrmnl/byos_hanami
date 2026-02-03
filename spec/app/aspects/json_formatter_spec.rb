6977cbde70130a077bb0286ce1a41e8858b35765
Brooke Kuhlmann <brooke@alchemists.io>
2026-02-03 13:44:00 -0700

    Added JSON formatter spec for arrays
    Necessary to demonstrate formatting for hashes _and_ arrays.
    
    Milestone: patch

--- spec/app/aspects/json_formatter_spec.rb
+++ spec/app/aspects/json_formatter_spec.rb
@@ -6,7 +6,19 @@ RSpec.describe Terminus::Aspects::JSONFormatter do
   subject(:formatter) { described_class }
 
   describe ".call" do
-    it "pretty prints JSON" do
+    it "pretty prints an array as JSON" do
+      data = [1, 2, 3]
+
+      expect(formatter.call(data)).to eq(<<~JSON.strip)
+        [
+          1,
+          2,
+          3
+        ]
+      JSON
+    end
+
+    it "pretty prints a hash as JSON" do
       data = {test: [{label: "Test"}]}
 
       expect(formatter.call(data)).to eq(<<~JSON.strip)
