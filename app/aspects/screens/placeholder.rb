24bf9ebad54772d3033887fb79ad994add31bfa6
Brooke Kuhlmann <brooke@alchemists.io>
2026-02-05 16:14:37 -0700

    Added screens placeholder
    Utilizes the Null Object Pattern to provide a safe fallback for screens that don't exist. This'll initially be used for devices and playlists that don't have an associated current screen.
    
    Milestone: minor

---
+++ app/aspects/screens/placeholder.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Aspects
+    module Screens
+      # A fallback (null object) for times when you need a screen that behaves like one but isn't.
+      Placeholder = Struct.new :id, :label, :name, :image_uri, :width, :height do
+        def initialize(
+          label: "Placeholder",
+          name: "placeholder",
+          image_uri: "/assets/setup.svg",
+          width: 800,
+          height: 480,
+          **
+        )
+          super
+        end
+      end
+    end
+  end
+end
---
+++ spec/app/aspects/screens/placeholder_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Aspects::Screens::Placeholder do
+  subject(:placeholder) { described_class.new }
+
+  describe "#initialize" do
+    it "answers default attributes" do
+      expect(placeholder).to have_attributes(
+        label: "Placeholder",
+        name: "placeholder",
+        image_uri: "/assets/setup.svg",
+        width: 800,
+        height: 480
+      )
+    end
+
+    it "answers custom attributes" do
+      attributes = {
+        id: 1,
+        label: "Test",
+        name: "test",
+        image_uri: "/test.svg",
+        width: 200,
+        height: 100
+      }
+
+      expect(described_class[**attributes]).to have_attributes(attributes)
+    end
+  end
+end
