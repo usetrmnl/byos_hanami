cab8ae3497f6045aeef93e5791fbd2e4cff6419d
Brooke Kuhlmann <brooke@alchemists.io>
2026-02-06 15:36:57 -0700

    Added popover screen content
    Necessary to provide a common scope for rendering popover screen content. The partial was renamed to reflect the new terminology. This is a breaking change which will require refactoring the associated templates to use this new scope. This'll be fixed in the next commit.
    
    Milestone: minor

---
+++ app/views/scopes/popover_screen_content.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Views
+    module Scopes
+      # Provides customized popover content.
+      class PopoverScreenContent < Hanami::View::Scope
+        def dom_id = "popover-screen-#{id}"
+
+        def width = locals.fetch __method__, 800
+
+        def height = locals.fetch __method__, 480
+
+        def render(path = "shared/popovers/content/screen") = super
+      end
+    end
+  end
+end
---
+++ spec/app/views/scopes/popover_screen_content_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Views::Scopes::PopoverScreenContent do
+  using Refinements::Hash
+
+  subject(:scope) { described_class.new locals:, rendering: Terminus::View.new.rendering }
+
+  let :locals do
+    Terminus::Aspects::Screens::Placeholder[id: 1].to_h.transform_keys! image_uri: :uri
+  end
+
+  describe "#dom_id" do
+    it "answers ID" do
+      expect(scope.dom_id).to eq("popover-screen-1")
+    end
+  end
+
+  describe "#width" do
+    it "answers default" do
+      locals.delete :width
+      expect(scope.width).to eq(800)
+    end
+  end
+
+  describe "#height" do
+    it "answers default" do
+      locals.delete :height
+      expect(scope.height).to eq(480)
+    end
+  end
+
+  describe "#render" do
+    it "renders content" do
+      assets = Hanami.app[:assets]
+
+      expect(scope.render).to eq(<<~CONTENT)
+        <dialog id="popover-screen-1"
+                class="bit-popover-content bit-popover-screen"
+                popover="auto">
+          <figure>
+            <img src="#{assets["setup.svg"]}" alt="Placeholder" width="800" height="480" loading="lazy">
+            <figcaption>Placeholder</figcaption>
+          </figure>
+
+          <p class="brand">
+            <img src="#{assets["logo/with_label.svg"]}" alt="Logo with Label" width="150" height="75">
+          </p>
+        </dialog>
+      CONTENT
+    end
+  end
+end
