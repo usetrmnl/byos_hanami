81bbc84919ff98d4a61e8d9e483abdf54298dd2e
Brooke Kuhlmann <brooke@alchemists.io>
2026-02-17 10:16:48 -0700

    Added device sensors synchronizer
    Necessary to synchronize sensor information across all devices but only when sensor information exists. This is most useful when sensors are connected to the server Terminus is hosted on like, for example, a Raspberry Pi.
    
    Milestone: minor

---
+++ app/aspects/devices/sensors/synchronizer.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require "dry/core"
+require "dry/monads"
+require "initable"
+require "refinements/array"
+require "refinements/hash"
+
+module Terminus
+  module Aspects
+    module Devices
+      module Sensors
+        # Creates or updates device sensor records based on hardware readings.
+        class Synchronizer
+          include Deps[
+            :settings,
+            :logger,
+            device_relation: "relations.device",
+            sensor_repository: "repositories.device_sensor"
+          ]
+          include Dry::Monads[:result]
+          include Initable[schema: proc { Terminus::Schemas::Devices::Sensors::Upsert }]
+
+          using Refinements::Array
+          using Refinements::Hash
+
+          def call = load.then { |data| process_devices data }
+
+          private
+
+          def load
+            path = settings.sensors_path
+
+            return JSON path.read if path.exist?
+
+            logger.info { "Sensors path not found: #{path}. Skipped." }
+            Dry::Core::EMPTY_ARRAY
+          end
+
+          def process_devices data
+            device_relation.select(:id)
+                           .map { it[:id] }
+                           .each { |id| process_sensors id, data }
+          end
+
+          def process_sensors device_id, data
+            data.fetch("data").map do |entry|
+              result = schema.call entry
+
+              if result.success?
+                deduplicate device_id, result.to_h
+              else
+                log_error result
+              end
+            end
+          end
+
+          def deduplicate device_id, attributes
+            if find_with device_id, attributes
+              logger.debug(tags: attributes) { "Duplicate sensor detected. Skipped." }
+            else
+              sensor_repository.create device_id:, source: "server", **attributes
+            end
+          end
+
+          def find_with device_id, attributes
+            attributes.transform_value!(:created_at) { Time.at(it).utc }
+
+            make, model, kind, created_at = attributes.values_at :make, :model, :kind, :created_at
+
+            sensor_repository.find_by device_id:,
+                                      make:,
+                                      model:,
+                                      kind:,
+                                      created_at:,
+                                      source: "device"
+          end
+
+          def log_error result
+            message = result.errors
+                            .to_h
+                            .map { |key, value| "#{key} #{value.to_sentence}" }
+                            .to_sentence delimiter: "; "
+
+            logger.error { "Unable to validate sensor: #{message}." }
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/app/aspects/devices/sensors/synchronizer_spec.rb
@@ -0,0 +1,143 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Aspects::Devices::Sensors::Synchronizer, :db do
+  using Refinements::Pathname
+
+  subject(:synchronizer) { described_class.new }
+
+  include_context "with application dependencies"
+
+  let(:repository) { Terminus::Repositories::DeviceSensor.new }
+  let(:path) { SPEC_ROOT.join "support/fixtures/sensors.json" }
+
+  before { allow(settings).to receive(:sensors_path).and_return(path) }
+
+  describe "#call" do
+    it "creates sensor records when they don't exist" do
+      device = Factory[:device]
+      synchronizer.call
+
+      expect(repository.all).to match(
+        array_including(
+          having_attributes(
+            device_id: device.id,
+            make: "Sensirion",
+            model: "SCD41",
+            kind: "carbon_dioxide",
+            source: "server",
+            created_at: Time.at(1735714800)
+          ),
+          having_attributes(
+            device_id: device.id,
+            make: "Sensirion",
+            model: "SCD41",
+            kind: "temperature",
+            source: "server",
+            created_at: Time.at(1735714800)
+          ),
+          having_attributes(
+            device_id: device.id,
+            make: "Sensirion",
+            model: "SCD41",
+            kind: "humidity",
+            source: "server",
+            created_at: Time.at(1735714800)
+          )
+        )
+      )
+    end
+
+    context "with duplicate sensors" do
+      let(:sensor) { Factory[:device_sensor] }
+
+      let :data do
+        {
+          data: [
+            {
+              make: sensor.make,
+              model: sensor.model,
+              kind: sensor.kind,
+              value: sensor.value,
+              unit: sensor.unit,
+              created_at: sensor.created_at.to_i
+            }
+          ]
+        }
+      end
+
+      before do
+        path = temp_dir.join("sensors.json").write(data.to_json)
+        allow(settings).to receive(:sensors_path).and_return(path)
+      end
+
+      it "logs duplicate" do
+        synchronizer.call
+
+        expect(logger.reread).to match(
+          /DEBUG.+Duplicate sensor detected. Skipped\..+make.+model.+kind.+value.+unit.+created_at/
+        )
+      end
+
+      it "skips sensor creation" do
+        expectation = proc { synchronizer.call }
+        count = proc { repository.all.size }
+
+        expect(&expectation).not_to change(&count)
+      end
+    end
+
+    context "with missing sensors path" do
+      let(:path) { temp_dir.join "sensors.json" }
+
+      it "doesn't upsert" do
+        expectation = proc { synchronizer.call }
+        count = proc { repository.all.size }
+
+        expect(&expectation).not_to change(&count)
+      end
+
+      it "logs synchronization was skipped" do
+        synchronizer.call
+        expect(logger.reread).to match(/INFO.+Sensors path not found: #{path}. Skipped./)
+      end
+    end
+
+    context "with invalid sensor attributes" do
+      let(:path) { temp_dir.join "sensors.json" }
+
+      before do
+        Factory[:device]
+
+        path.write <<~JSON
+          {
+            "data": [
+              {
+                "make": "Sensirion",
+                "model": "SCD41",
+                "kind": "co2",
+                "created_at": 1771339473
+              }
+            ]
+          }
+        JSON
+      end
+
+      it "doesn't upsert" do
+        expectation = proc { synchronizer.call }
+        count = proc { repository.all.size }
+
+        expect(&expectation).not_to change(&count)
+      end
+
+      it "logs synchronization was skipped" do
+        synchronizer.call
+
+        expect(logger.reread).to match(
+          /ERROR.+Unable to validate sensor: value is missing and unit is missing\./
+        )
+      end
+    end
+  end
+end
