fc77191887a14a4d55593ef7fb8999d9fc1df61f
Brooke Kuhlmann <brooke@alchemists.io>
2026-02-18 15:31:58 -0700

    Added device sensor struct
    Necessary to enhance default functionality by, initially, providing a way to obtain attributes for use in Liquid templates.
    
    Milestone: minor

---
+++ app/structs/device_sensor.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "refinements/hash"
+
+module Terminus
+  module Structs
+    # The device sensor struct.
+    class DeviceSensor < DB::Struct
+      using Refinements::Hash
+
+      def liquid_attributes
+        {make:, model:, kind:, value:, unit:, source:, created_at:}.stringify_keys!
+      end
+    end
+  end
+end
---
+++ spec/app/structs/device_sensor_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Structs::DeviceSensor do
+  subject(:sensor) { Factory.structs[:device_sensor] }
+
+  describe "#liquid_attributes" do
+    it "answers attributes" do
+      expect(sensor.liquid_attributes).to eq(
+        "kind" => "temperature",
+        "make" => "ACME",
+        "model" => "Test",
+        "value" => 20.1,
+        "unit" => "celcius",
+        "source" => "device",
+        "created_at" => Time.new(2025, 1, 1).utc
+      )
+    end
+  end
+end
