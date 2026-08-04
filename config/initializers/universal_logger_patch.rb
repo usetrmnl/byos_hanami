df2c842eb5622238b5eb9270977c8d828f227153
Brooke Kuhlmann <brooke@alchemists.io>
2026-08-04 14:05:46 -0600

    Fixed universal logger to merge block content with payload
    Solves situations where keys can be missing for both the Rack and SQL formats. This also simplifies the patch by mimicking the original implementation by merging the block content into the payload.
    
    The SQL check for block content being a hash is no longer required either.
    
    Issue: 348
    Milestone: patch

--- config/initializers/universal_logger_patch.rb
+++ config/initializers/universal_logger_patch.rb
@@ -3,13 +3,25 @@
 
 # Patches Hanami's universal logger to work with Cogger.
 module UniversalLoggerPatch
+  DEFAULTS = {
+    verb: nil,
+    status: nil,
+    elapsed: nil,
+    elapsed_unit: nil,
+    ip: nil,
+    path: nil,
+    length: nil,
+    db: nil,
+    query: nil
+  }.freeze
+
   def _log_structured method, message, payload
     tags = _current_tags
     block_content = yield if block_given?
 
     logger.formatter = case tags
                          in [:rack] then tags.delete :rack
-                         in [:sql] if block_content.is_a? Hash then tags.delete :sql
+                         in [:sql] then tags.delete :sql
                          else Hanami.env == :development ? :emoji : :json
                        end
 
@@ -18,7 +30,9 @@ module UniversalLoggerPatch
       block_content = {}
     end
 
-    logger.public_send method, message, tags:, **block_content.merge!(payload)
+    combined = {**DEFAULTS, **payload, **block_content}
+
+    logger.public_send method, message, tags:, **combined
   end
 end
 
