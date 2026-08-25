5277625811a7985b723126f1a204b6055f853968
Brooke Kuhlmann <brooke@alchemists.io>
2026-08-25 10:42:06 -0600

    Added screen interrupt show view
    Necessary to define a parent class that other classes can inherit from which reduces duplication of having to define the same layout across multiple classes. Existing views that use the `interrupt` layout will soon be refactored to inherit from this class instead.
    
    Issue: 345
    Milestone: minor

---
+++ app/views/screens/interrupts/show.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Views
+    module Screens
+      module Interrupts
+        # The show view.
+        class Show < View
+          config.layout = "interrupt"
+        end
+      end
+    end
+  end
+end
