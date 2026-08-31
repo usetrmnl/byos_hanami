bfb24e6003e40cbe785dc49c59cc47ab99db3f33
Brooke Kuhlmann <brooke@alchemists.io>
2026-08-31 16:51:25 -0600

    Added Superfluid filters container
    Necessary to register all dependencies for registration as Superfluid filters.
    
    Milestone: minor

---
+++ app/aspects/superfluid/filters/container.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "containable"
+
+module Terminus
+  module Aspects
+    module Superfluid
+      module Filters
+        # Registers dependencies.
+        module Container
+          extend Containable
+
+          register :append_random, AppendRandom
+          register :days_ago, DaysAgo
+          register :find_by, FindBy
+          register :parse_json, FromJSON
+          register :group_by, GroupBy
+          register :map_to_i, MapToI
+          register(:markdown_to_html) { Markdown.new }
+          register(:number_to_currency) { NumberToCurrency.new }
+          register(:number_with_delimiter) { NumberWithDelimiter.new }
+          register(:ordinalize) { Ordinalize.new }
+          register :pluralize, Pluralize
+          register :qr_code, QRCode
+          register :random_number, RandomNumber
+          register :sample, Sample
+          register :json, ToJSON
+        end
+      end
+    end
+  end
+end
