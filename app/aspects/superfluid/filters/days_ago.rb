af85540d1c8d3685c6c73204dccf4b9f1b24ff5f
Brooke Kuhlmann <brooke@alchemists.io>
2026-09-01 09:11:41 -0600

    Fixed Superfluid days ago spec to be more stable
    Ensures specs don't randomly fail due to running at different times by using a constant date for testing purposes. The implementation was also refactored to use clearer parameter names for improved readability.
    
    Milestone: patch

--- app/aspects/superfluid/filters/days_ago.rb
+++ app/aspects/superfluid/filters/days_ago.rb
@@ -6,8 +6,8 @@ module Terminus
   module Aspects
     module Superfluid
       module Filters
-        DaysAgo = lambda do |value, timezone = "Etc/UTC", info: TZInfo::Timezone|
-          info.get(timezone).now.to_date - value.to_i
+        DaysAgo = lambda do |value, zone = "Etc/UTC", timezone: TZInfo::Timezone|
+          timezone.get(zone).now.to_date - value.to_i
         end
       end
     end
--- spec/app/aspects/superfluid/filters/days_ago_spec.rb
+++ spec/app/aspects/superfluid/filters/days_ago_spec.rb
@@ -6,12 +6,19 @@ RSpec.describe Terminus::Aspects::Superfluid::Filters::DaysAgo do
   subject(:filter) { described_class }
 
   describe "#call" do
+    let(:timezone) { class_double TZInfo::Timezone }
+    let(:data_time_zone) { instance_double TZInfo::DataTimezone, now: time_with_offset }
+    let(:time_with_offset) { instance_double TZInfo::TimeWithOffset, to_date: date }
+    let(:date) { Date.parse "2026-09-01" }
+
     it "answers days for default timezone" do
-      expect(filter.call(3)).to eq(Date.today - 3)
+      allow(timezone).to receive(:get).with("Etc/UTC").and_return(data_time_zone)
+      expect(filter.call(3, timezone:)).to eq(date - 3)
     end
 
     it "answers days for custom timezone" do
-      expect(filter.call(10, "Europe/London")).to eq(Date.today - 9)
+      allow(timezone).to receive(:get).with("Europe/London").and_return(data_time_zone)
+      expect(filter.call(3, "Europe/London", timezone:)).to eq(date - 3)
     end
   end
 end
