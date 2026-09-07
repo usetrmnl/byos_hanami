592d8ac6a14be8cc43ccc9aa025e2239f175786a
Brooke Kuhlmann <brooke@alchemists.io>
2026-09-07 15:17:35 -0600

    Added extension patch contract
    Necessary to validate patch requests.
    
    Milestone: minor

---
+++ app/contracts/extensions/patch.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Contracts
+    module Extensions
+      # The contract for extension patches.
+      class Patch < Contract
+        config.messages.namespace = :extension
+
+        params do
+          required(:id).filled :integer
+          required(:extension).filled Schemas::Extensions::Patch
+          optional(:model_ids).array :int?
+          optional(:device_ids).array :int?
+        end
+
+        rule extension: :interval, &Rules::Cron
+      end
+    end
+  end
+end
